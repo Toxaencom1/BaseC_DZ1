32a55fcfce3fe6a3efa1a505f3616ecb18ce18c7
Toxaencom1 <Toxaencom@gmail.com>
2023-10-27 19:26:42 +0600

    Add task text

--- DZ1.c
+++ DZ1.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
+
 int main()
-{
-    printf("------------\n");
+{    
+    printf("------------Задание 1. Вывести на экран “Hello world!”\n");
     printf("Hello world!\n");
-    printf("------------\n");
+    printf("------------Задание 2. Вывести на экран текст \"лесенкой\"\n");
     printf("\n");
     printf("Let's\n  go\n    to walk\n");
-    printf("------------\n");
+    printf("------------Задание 3. Вывести на экран рисунок из букв\n");
     printf("   *   \n");
     printf("  ***  \n");
     printf(" ***** \n");
     printf("*******\n");
     printf(" HH HH \n");
     printf(" ZZZZZ \n");
-    printf("------------\n");
+    printf("------------Задание 4*. Нарисовать соты\n");
     printf(" __    __ \n");
     printf("/  \\__/  \\\n");
     printf("\\__/  \\__/\n");
