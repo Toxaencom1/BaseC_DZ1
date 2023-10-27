1bbc2549e213e27c52823f28cbaa6a96d16aae5b
Toxaencom1 <Toxaencom@gmail.com>
2023-10-27 19:12:29 +0600

    Solved DZ1 for BaseC

---
+++ DZ1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("------------\n");
+    printf("Hello world!\n");
+    printf("------------\n");
+    printf("\n");
+    printf("Let's\n  go\n    to walk\n");
+    printf("------------\n");
+    printf("   *   \n");
+    printf("  ***  \n");
+    printf(" ***** \n");
+    printf("*******\n");
+    printf(" HH HH \n");
+    printf(" ZZZZZ \n");
+    printf("------------\n");
+    printf(" __    __ \n");
+    printf("/  \\__/  \\\n");
+    printf("\\__/  \\__/\n");
+    printf("/  \\__/  \\\n");
+    printf("\\__/  \\__/\n");
+    printf("------------\n");
+    return 0;
+}
